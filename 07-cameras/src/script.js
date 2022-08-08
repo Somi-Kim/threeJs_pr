886d37b714353e1250ce67ff9720a5567d56c42d
Somi-Kim <kimsnack0604@naver.com>
2022-08-08 16:03:22 +0900

    08 excercise

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,7 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js' // THREE.OrbitControls >> OrbitControls
 /*
 Cursor
 */
